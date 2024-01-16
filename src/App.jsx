f51170709d9dffd857e168a0a21813e44a9b206a
hopeadoli <adolihope@gmail.com>
2024-01-16 21:17:30 +0000

    added meta details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { Helmet } from "react-helmet";
 import Navbar from "./Navbar";
 import About from "./components/About/About";
 import Gallery from "./components/Gallery/Gallery";
 import Home from "./components/Home/Home";
 import NoPage from "./components/NoPage";
 import { Route, Routes } from "react-router-dom";
+import ogImage from "./assets/og-image.png";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta property="og:title" content="Hope Adoli" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://uxkafui.co" />
+        <meta property="og:image" content={ogImage} />
+        <meta
+          property="og:description"
+          content="Product Designer, Developer, Artist, 
+Volunteer. An Individual Member of the Django Software Foundation."
+        />
+      </Helmet>
       <Navbar />
       <div>
         <Routes>
