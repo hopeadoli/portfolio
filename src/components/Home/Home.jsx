217fc2c7e57b927978459d336b7c79d108f97455
hopeadoli <adolihope@gmail.com>
2024-01-16 20:46:51 +0000

    added meta details

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,32 +7,6 @@ export default function Home() {
   return (
     <>
       <Helmet>
-        {/* Facebook / OpenGraph */}
-        <meta property="og:title" content="Hope Adoli" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://uxkafui.co" />
-        <meta
-          property="og:image"
-          content="https://uxkafui.co/src/assets/og-image.png"
-        />
-        <meta
-          property="og:description"
-          content="Product Designer, Developer, Artist, 
-    Volunteer. An Individual Member of the Django Software Foundation."
-        />
-
-        {/* Twitter */}
-        <meta name="twitter:card" value="summary_large_image" />
-        <meta name="twitter:site" value="@uxkafui" />
-        <meta name="twitter:url" value="https://uxkafui.co" />
-        <meta name="twitter:title" value="Hope Adoli" />
-        <meta
-          name="twitter:description"
-          value="Product Designer, Developer, Artist, 
-    Volunteer. An Individual Member of the Django Software Foundation."
-        />
-        <meta name="twitter:image" content="https://uxkafui.co/og-image.png" />
-
         <meta charSet="utf-8" />
         <title>Hope Adoli</title>
         <link rel="canonical" href="https://uxkafui.co" />
