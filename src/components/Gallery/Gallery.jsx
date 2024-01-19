313238acb016dc8e90b45ce2b149d1abc5031534
hopeadoli <adolihope@gmail.com>
2024-01-19 12:15:02 +0000

    fixes on the meta data

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -9,7 +9,22 @@ export default function Gallery() {
       <Helmet>
         <meta charSet="utf-8" />
         <title>The Yeʋekpɔme Gallery by Hope Adoli</title>
-        <link rel="canonical" href="https://uxkafui.co" />
+        <link rel="canonical" href="https://uxkafui.co/gallery" />
+
+        <meta
+          property="og:title"
+          content="The Yeʋekpɔme Gallery by Hope Adoli"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://uxkafui.co/gallery" />
+        <meta
+          property="og:image"
+          content="https://uxkafui.co/assets/og-image-iULTaCta.png"
+        />
+        <meta
+          property="og:description"
+          content="The Yeʋekpɔme Gallery ih home to paintings by Hope Adoli"
+        />
       </Helmet>
       <ForeText />
       <Images />
