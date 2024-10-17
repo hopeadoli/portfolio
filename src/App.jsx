831ba3f03ffc6defc0829928ae9184e4903e0228
hopeadoli <adolihope@gmail.com>
2024-10-17 19:14:25 +0000

    fixed issue with scroll location on pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Gallery from "./components/Gallery/Gallery";
 import Home from "./components/Home/Home";
 import NoPage from "./components/NoPage";
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
